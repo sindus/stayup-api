f1b668760a8c161f022042418ab216187b379cb3
Sikander Ravate <sikander.ravate@gmail.com>
2026-03-15 01:32:04 +0100

    remove debug endpoints

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,8 @@ import { connectorsRoute } from './routes/connectors.js'
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.onError((err, c) => {
-  console.error(err)
-  return c.json({ error: err.message, stack: err.stack }, 500)
-})
-
 app.route('/auth', authRoute)
 app.get('/', (c) => c.json({ status: 'ok' }))
-app.get('/debug-env', (c) => c.json({ keys: Object.keys(c.env ?? {}), db: !!c.env?.DATABASE_URL, jwt: !!c.env?.JWT_SECRET }))
 app.route('/connectors', connectorsRoute)
 
 export default app
